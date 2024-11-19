97a1329f7d6b9144c52a2510f2a787db7f2c434e
Kratenko Ivan <kratenkovanya@gmail.com>
2024-11-19 15:12:13 +0200

    created react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App(){
+    return(
+        <div style={{textAlign: 'center'}}>
+            <h1>Forum</h1>
+            <p style={{fontSize: '20px'}}>This is very cool forum</p>
+            <img src="https://papik.pro/uploads/posts/2022-08/1661918702_2-papik-pro-p-smailik-laik-pokazivaet-png-2.jpg" />
+        </div>
+    )
 }
-
-export default App;
