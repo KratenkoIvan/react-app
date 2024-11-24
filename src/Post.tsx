bea77df314f84f337b678358e734cf3fef63caa1
Kratenko Ivan <kratenkovanya@gmail.com>
2024-11-24 21:14:04 +0200

    added useState & likes for posts

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 interface IPostProps {
     title: string
     description: string
@@ -6,12 +8,21 @@ interface IPostProps {
 }
 
 export function Post(props: IPostProps){
+    const [likes, setLike] = useState(0)
+    const [isButtonDisabled, setButtonDisabled] = useState(false)
+    function addLike(){
+        setLike(likes + 1)
+        setButtonDisabled(true)
+    }
     return(
         <div>
             <h1>{props.title}</h1>
             <p>{props.description}</p>
             <img src={props.image}/>
             <p>{props.author}</p>
+            <p>Likes: {likes}</p>
+            <button onClick={addLike} disabled = {isButtonDisabled} style={{width: '50px', height: '30px', fontSize: '18px'}}>👍</button>
+            <hr />
         </div>
     )
 }
